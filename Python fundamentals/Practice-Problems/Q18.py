1962b4a5a5b461b05fbfbc9eaddbc9c75234ce58
Tanyajain2006 <jaintanya2006@gmail.com>
2026-05-22 13:42:47 +0530

    Q19 done

--- Python fundamentals/Practice-Problems/Q18.py
+++ Python fundamentals/Practice-Problems/Q18.py
@@ -1,9 +1,16 @@
 num = (int)(input("Enter a number: "))
 
 sum = 0; temp = num
+
+cnt = 0
+while temp > 0:
+    cnt += 1
+    temp //= 10
+
+temp = num
 while temp > 0:
     digit = temp % 10
-    sum += digit ** 2
+    sum += digit ** cnt
     temp //= 10
 
 if num == sum: print(num, "is a Armstrong Number")
